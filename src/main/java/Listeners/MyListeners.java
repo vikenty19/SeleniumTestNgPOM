211141872f9af0f45d653cb3a5fdebd7c544a504
vikenty19 <vikenty.plakhov@testpro.io>
2024-11-20 15:47:17 -0500

    listeners implemented

--- src/main/java/Listeners/MyListeners.java
+++ src/main/java/Listeners/MyListeners.java
@@ -6,33 +6,41 @@ import org.testng.ITestResult;
 
 public class MyListeners implements ITestListener{
 
-
+    @Override
+    public void onStart(ITestContext context) {
+        System.out.println("Execution started");
+    }
     @Override
     public void onTestStart(ITestResult result) {
-
+      String testName= result.getName();
+        System.out.println(testName+"   started executing");
     }
 
     @Override
     public void onTestSuccess(ITestResult result) {
+        String  testName = result.getName();
+        System.out.println(testName + "    got executed successfully");
 
     }
 
     @Override
     public void onTestFailure(ITestResult result) {
-
+        String testName= result.getName();
+        System.out.println(testName+"  got failed!");
+        System.out.println(result.getThrowable());
     }
 
     @Override
     public void onTestSkipped(ITestResult result) {
-
+        String testName= result.getName();
+        System.out.println(testName+"  got skipped");
+        System.out.println(result.getThrowable());
     }
 
-    @Override
-    public void onStart(ITestContext context) {
-        ;
-    }
+
 
     @Override
     public void onFinish(ITestContext context) {
+        System.out.println("Execution is completed!");
     }
 }
