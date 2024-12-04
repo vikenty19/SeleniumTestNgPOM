69838044da58fd223f77503df7aa848c378cf855
vikenty19 <vikenty.plakhov@testpro.io>
2024-12-04 10:20:14 -0500

    .

--- src/test/java/experiments/Person.java
+++ src/test/java/experiments/Person.java
@@ -3,7 +3,7 @@ package experiments;
 import java.io.Serializable;
 
 public class Person implements Serializable {
-    private int id;
+    private  int id;
     private   String name;//transient - for omitting from writing this field down
     public Person(int id, String name){
         this.id =id;
